1e44b10006c0b9e55b4da9df403d631889182abb
abadia <abadia@norsys.fr>
2025-07-11 19:45:09 +0100

    Configure router and create login and signup views

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -2,16 +2,22 @@ import { useState } from "react";
 import Backdrop from "../components/backdrop/Backdrop";
 import Navbar from "../components/navbar/Navbar";
 import Sidebar from "../components/sidebar/Sidebar";
+import { Outlet } from "react-router-dom";
+import styles from './Layout.module.css'; // Create this file
 
 const Layout = () => {
-const [show,handleSetShow] = useState(false)
-return (
-    <>
-     <Navbar handleOpenSideBar={() => handleSetShow(true)}  />
-     <Sidebar showSideBar={show} handleCloseSideBar={()=> handleSetShow(false)} />
-     {show && <Backdrop/>}
-    </>
-)
-}
+    const [show, handleSetShow] = useState(false);
+
+    return (
+        <>
+            <Navbar handleOpenSideBar={() => handleSetShow(true)} />
+            <Sidebar showSideBar={show} handleCloseSideBar={() => handleSetShow(false)} />
+            {show && <Backdrop />}
+            <main className={styles['outlet-container']}>
+                <Outlet />
+            </main>
+        </>
+    );
+};
 
 export default Layout;
