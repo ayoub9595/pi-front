13a2abbac30f07740d2e71140b6c27dc2544c32c
Ayoub Badiâ <ayoub.badia95@gmail.com>
2025-07-26 20:49:22 +0100

    Add Loader Component

--- src/views/equipment/equipmentList/EquipmentsList.jsx
+++ src/views/equipment/equipmentList/EquipmentsList.jsx
@@ -6,10 +6,12 @@ import EditIcon from "../../../components/icons/EditIcon.jsx";
 import DeleteIcon from "../../../components/icons/DeleteIcon.jsx";
 import ConfirmModal from "../../../components/confirmModal/CofirmModal.jsx";
 import {toast} from "react-hot-toast";
+import Loader from "../../../components/loader/Loader.jsx";
 
 
 const EquipmentsList = () => {
     const [equipments, setEquipments] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
     const navigate = useNavigate();
     const [showConfirm, setShowConfirm] = useState(false);
     const [selectedId, setSelectedId] = useState(null);
@@ -20,20 +22,25 @@ const EquipmentsList = () => {
 
     const fetchAllEquipments = async () => {
         try {
+            setIsLoading(true);
             const data = await getEquipments();
             setEquipments(data);
         } catch (err) {
             toast.error(err.message || "Erreur lors du chargement des équipements",{duration: 2000});
+        } finally {
+            setIsLoading(false);
         }
     };
 
     const handleUpdate = (id) => {
         navigate(`/home/equipements/edit/${id}`);
     };
+
     const handleDelete = (id) => {
         setSelectedId(id);
         setShowConfirm(true)
     }
+
     const confirmDelete = async () => {
         try {
             await deleteEquipment(selectedId);
@@ -49,7 +56,7 @@ const EquipmentsList = () => {
             })
             setSelectedId(null);
         } catch (err) {
-            toast.error(err.message || "Erreur lors de la suppression de l’équipement",{duration: 2000});
+            toast.error(err.message || "Erreur lors de la suppression de l'équipement",{duration: 2000});
         }
     };
 
@@ -57,71 +64,89 @@ const EquipmentsList = () => {
         setShowConfirm(false);
         setSelectedId(null);
     };
-    return (
-        equipments.length > 0 ? (
-                <div className={styles["table-container"]}>
-                    {showConfirm && <ConfirmModal
-                        title="Attention !"
-                        message="Voulez-vous supprimer cet equipement ?"
-                        handleConfirm={confirmDelete}
-                        handleCancel={cancelDelete}
-                    />
-                    }
-                    <div className={styles.header}>
-                        <h2>Liste des Équipements</h2>
-                        <div>
-                            <button className={styles['add-button']} onClick={() => navigate('/home')}>➕</button>
-                        </div>
+
+    if (isLoading) {
+        return <Loader />;
+    }
+
+    if (equipments.length === 0) {
+        return (
+            <div className={styles["no-equipment-container"]}>
+                <div className={styles.header}>
+                    <h2>Liste des Équipements</h2>
+                    <div>
+                        <button className={styles['add-button']} onClick={() => navigate('/home')}>➕</button>
                     </div>
-
-                    <table className={styles.table}>
-                        <thead>
-                        <tr>
-                            <th>Nom</th>
-                            <th>Description</th>
-                            <th>Numéro de Série</th>
-                            <th>Date d'Acquisition</th>
-                            <th>Maintenance Prévue</th>
-                            <th>Actif</th>
-                            <th>Caractéristiques</th>
-                            <th>Actions</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        {equipments.map((eq) => (
-                            <tr key={eq.id}>
-                                <td>{eq.nom}</td>
-                                <td>{eq.description || "-"}</td>
-                                <td>{eq.numero_serie || "-"}</td>
-                                <td>{eq.date_acquisition?.split("T")[0] || "-"}</td>
-                                <td>{eq.maintenance_prevue?.split("T")[0] || "-"}</td>
-                                <td>{eq.est_actif ? "Oui" : "Non"}</td>
-                                <td>
-                                    <ul style={{ paddingLeft: "16px", margin: 0 }}>
-                                        {eq.caracteristiques.map((carac, index) => (
-                                            <li key={index}>
-                                                <strong>{carac.caracteristique}</strong>: {carac.valeur}
-                                            </li>
-                                        ))}
-                                    </ul>
-                                </td>
-                                <td>
-                                    <div className={styles["action-buttons"]}>
-                                        <button onClick={() => handleUpdate(eq.id)} className={`${styles["action-button"]} ${styles["edit-btn"]}`}>
-                                            <EditIcon />
-                                        </button>
-                                        <button onClick={() => handleDelete(eq.id)} className={`${styles["action-button"]} ${styles["delete-btn"]}`}>
-                                            <DeleteIcon />
-                                        </button>
-                                    </div>
-                                </td>
-                            </tr>
-                        ))}
-                        </tbody>
-                    </table>
                 </div>
+                <div className={styles["no-equipment-message"]}>
+                    <p>Aucun équipement disponible. Veuillez ajouter un.</p>
+                </div>
+            </div>
+        );
+    }
 
-            ): <p>Loading ...</p>
+    return (
+        <div className={styles["table-container"]}>
+            {showConfirm && <ConfirmModal
+                title="Attention !"
+                message="Voulez-vous supprimer cet equipement ?"
+                handleConfirm={confirmDelete}
+                handleCancel={cancelDelete}
+            />
+            }
+            <div className={styles.header}>
+                <h2>Liste des Équipements</h2>
+                <div>
+                    <button className={styles['add-button']} onClick={() => navigate('/home')}>➕</button>
+                </div>
+            </div>
+
+            <table className={styles.table}>
+                <thead>
+                <tr>
+                    <th>Nom</th>
+                    <th>Description</th>
+                    <th>Numéro de Série</th>
+                    <th>Date d'Acquisition</th>
+                    <th>Maintenance Prévue</th>
+                    <th>Actif</th>
+                    <th>Caractéristiques</th>
+                    <th>Actions</th>
+                </tr>
+                </thead>
+                <tbody>
+                {equipments.map((eq) => (
+                    <tr key={eq.id}>
+                        <td>{eq.nom}</td>
+                        <td>{eq.description || "-"}</td>
+                        <td>{eq.numero_serie || "-"}</td>
+                        <td>{eq.date_acquisition?.split("T")[0] || "-"}</td>
+                        <td>{eq.maintenance_prevue?.split("T")[0] || "-"}</td>
+                        <td>{eq.est_actif ? "Oui" : "Non"}</td>
+                        <td>
+                            <ul style={{ paddingLeft: "16px", margin: 0 }}>
+                                {eq.caracteristiques.map((carac, index) => (
+                                    <li key={index}>
+                                        <strong>{carac.caracteristique}</strong>: {carac.valeur}
+                                    </li>
+                                ))}
+                            </ul>
+                        </td>
+                        <td>
+                            <div className={styles["action-buttons"]}>
+                                <button onClick={() => handleUpdate(eq.id)} className={`${styles["action-button"]} ${styles["edit-btn"]}`}>
+                                    <EditIcon />
+                                </button>
+                                <button onClick={() => handleDelete(eq.id)} className={`${styles["action-button"]} ${styles["delete-btn"]}`}>
+                                    <DeleteIcon />
+                                </button>
+                            </div>
+                        </td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+        </div>
     );
 };
 
